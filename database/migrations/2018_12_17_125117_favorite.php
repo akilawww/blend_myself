8dacddbb4d42a47c2d71c7dacbebf5426f50027d
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2018-12-18 09:48:32 +0900

    [New] Create favorite table

---
+++ database/migrations/2018_12_17_125117_favorite.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BlendMyself extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorite', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->foreign('recipe_id')
+                ->references('id')->on('recipes')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorite');
+    }
+}
