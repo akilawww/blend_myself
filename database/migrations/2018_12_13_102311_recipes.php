4dca36ebeae9ada99612de8772dfd32b1464928a
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2018-12-18 09:42:13 +0900

    [New] Create recipes table

---
+++ database/migrations/2018_12_13_102311_recipes.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BlendMyself extends Migration
+{
+    /**
+     * Run the migrations.
+     * レシピテーブルの生成
+     * 
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('overview');
+            $table->string('image');
+            $table->string('appetizers');
+            $table->string('degree');
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
