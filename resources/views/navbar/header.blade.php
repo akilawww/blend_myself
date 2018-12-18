6ae692ca83cfbcdb996a6f4aecddfb9c1fdcbbf1
KawasakiHachi <kd1221092@st.kobedenshi.ac.jp>
2018-12-18 14:52:07 +0900

    header&footer

--- resources/views/navbar/header.blade.php
+++ resources/views/navbar/header.blade.php
@@ -1,36 +1,41 @@
 <html lang="{{ app()->getLocale() }}">
   <head>
-    <title>アプリ名 - @yield('title')</title>
+    <title>さけばさだー</title>
   </head>
-  <body>
+<body>
+<nav class="navbar navbar-default fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
 
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <!-- data-target="#togglebar"で、ハンバーガーメニューを選択するとメニューリンクを表示する-->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#togglebar">
-                    <!--以下からハンバーガーメニューのhtml要素 画面が縮小されると表示-->
-                    <span class="sr-only">Toggle navigation</span>
-                    <!--icon-barはBootstrapのcss -->
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">さけばさだー</a>
-            </div>
-            <!--メニューの要素-->
-            <div class="collapse navbar-collapse" id="togglebar">
-                <ul class="nav navbar-nav">
-                    <!--以下でページ内リンクを設定している-->
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-
-                </ul>
-            </div>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="index.php"><img src="{{ asset('image/sakebasada.png') }}"></a></li>
+    </ul>
+    <form class="navbar-form navbar-left">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search">
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
         </div>
-    </nav>
-    @section('head')
-      test
-    @show
-  </body>
-</html>
+      </div>
+    </form>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+
+  </div>
+</nav>
+
+@yield('content')
+
+<hr>
+<ul class="list-group">
+    <li><a href="">ガイドライン</a></li>
+    <li><a href="">個人情報の取り扱いについて</a></li>
+    <li><a href="">当サイトご利用の際の規約</a></li>
+    <li><a href="">ヘルプ</a></li>
+    <li><a href="">お問い合わせ</a></li>
+</ul>
