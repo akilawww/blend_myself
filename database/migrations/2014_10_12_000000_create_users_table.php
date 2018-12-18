e0e0e530fc889a399b6be387067f5acdab959eda
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2018-12-18 09:37:41 +0900

    [Change] users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('icon')->default('');
+            $table->string('comment')->defult('');
+            $table->string('birthday')->defult('');
             $table->rememberToken();
             $table->timestamps();
         });
