6f25621d64fd84101b6d0fb49ce078afeda9eebe
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2019-01-10 14:34:59 +0900

    [Add] Link add recipeform

--- resources/views/navbar/header.blade.php
+++ resources/views/navbar/header.blade.php
@@ -53,6 +53,7 @@
                             document.getElementById('logout-form').submit();">
                 {{ __('Logout') }}
             </a>
+            <a class="dropdown-item text-light" href="/recipe_form">レシピ作成</a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
