cc571b61bc29b17cba0b901e820412c4c2bdc552
KawasakiHachi <kd1221092@st.kobedenshi.ac.jp>
2019-01-17 14:14:49 +0900

    [Change] show body->col-8

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -21,22 +21,21 @@
             更新日：{{ $recipe->updated_at->format('Y年m月d日　H時m分') }}
         @endif
     </div>
-    <div class="card-horizon">
-        <div class="row card-horizon-con">
-            <div class="left">
+        <div class="row">
+            <div class="left col-md-4">
                 <img src="{{ asset($recipe->image) }}" alt="Sample">
             </div>
-            <div class="">
-                {!! nl2br(e($recipe->body)) !!}
-            </div>
-        </div>
-        </div>
-            <ul class="list-group" style="max-width: 400px;">
+            <div class="right col-md-8">
+                {!! nl2br(e($recipe->body)) !!}<br>
+                <ul class="list-group" style="max-width: 400px;">
                 @foreach ($materrials as $materrial)
                 <li class="list-group-item">{{ $materrial->name }}： {{ $materrial->quantity }} 
                     {{ empty($materrial->degree) ? '' : $materrial->degree }}</li>
                 @endforeach
             </ul>
+            </div>
+        </div>
+            
     @foreach ($recipe_procedures as $recipe_procedure)
         <div class="card left" style="width: 20rem;">
             <img class="card-img-top" src="{{ asset($recipe_procedure->image) }}" alt="Sample">
