c5cdd71c8f02588fd495beb6212a11faec0c8196
KawasakiHachi <kd1221092@st.kobedenshi.ac.jp>
2019-01-18 13:07:51 +0900

    [Change] Error center

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -9,14 +9,12 @@
 @include('navbar.header')
 
 @section('content')
-<div class="container">
-  <span class="Target">
+<center>
     <img src="{{ asset('/image/gummy.png') }}">
     <h1>{{ $title }}</h1>
     <p><strong>{{ __('Error') }}: <span class="error-code">403</span></strong></p>
     <a href="{{ url('/')}}" class="text-center">{{ __('Topへ戻る') }}</a>
-  </span>
-</div>
+</center>
 @endsection
 
 @include('navbar.footer')
