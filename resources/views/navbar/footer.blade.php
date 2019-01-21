d46ba871d81c747dcb1d189dbc74c14bce279077
KawasakiHachi <kd1221092@st.kobedenshi.ac.jp>
2019-01-21 15:08:37 +0900

    [Change] footer list

--- resources/views/navbar/footer.blade.php
+++ resources/views/navbar/footer.blade.php
@@ -2,12 +2,15 @@
 
 @section('footer')
   <footer class="bg-dark footer text-light">
-    <ul class="list-group">
-      <span><a href="{{URL('/guideline')}}" class="text-light">ガイドライン</a></span>
-      <span><a href="{{URL('/privacypolicy')}}" class="text-light">個人情報の取り扱いについて</a></span>
-      <span><a href="{{URL('/serviceterms')}}" class="text-light">当サイトご利用の際の規約</a></span>
-      <span><a href="" class="text-light">ヘルプ</a></span>
+  	<center>
+    <ul style="padding-top: 1rem;">
+      <span><a href="{{URL('/guideline')}}" class="text-light">ガイドライン</a>　|　</span>
+      <span><a href="{{URL('/privacypolicy')}}" class="text-light">個人情報の取り扱いについて</a>　|　</span>
+      <span><a href="{{URL('/serviceterms')}}" class="text-light">当サイトご利用の際の規約</a>　|　</span>
+      <span><a href="" class="text-light">ヘルプ</a>　|　</span>
       <span><a href="" class="text-light">お問い合わせ</a></span>
     </ul>
+    <ul>こぴーらいと</ul>
+  </center>
   </footer>
 @endsection
