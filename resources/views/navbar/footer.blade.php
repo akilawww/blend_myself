f05f0cb46b37bca394b0c79f5e0c1306c3e22bae
yokotakento <yokoken6164@gmail.com>
2019-01-21 10:47:20 +0900

    ガイドラインへのリンク追加

--- resources/views/navbar/footer.blade.php
+++ resources/views/navbar/footer.blade.php
@@ -3,7 +3,7 @@
 @section('footer')
   <footer class="bg-dark footer text-light">
     <ul class="list-group">
-      <span><a href="" class="text-light">ガイドライン</a></span>
+      <span><a href="{{URL('/guideline')}}" class="text-light">ガイドライン</a></span>
       <span><a href="" class="text-light">個人情報の取り扱いについて</a></span>
       <span><a href="" class="text-light">当サイトご利用の際の規約</a></span>
       <span><a href="" class="text-light">ヘルプ</a></span>
