124df04725fc4416a684abafbdb366eb3cc6a707
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2019-01-21 14:08:16 +0900

    [New] Create Function reMakeImagePath

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -5,3 +5,9 @@
         array_splice($ary, 0, 1, array('storage')); // publicをstorageに変換
         return implode('/', $ary);
     }
+    
+    function reMakeImagePath($path) {
+        $ary = explode('/', $path);
+        array_splice($ary, 0, 1, array('public')); // storageをpublicに変換
+        return implode('/', $ary);
+    }
