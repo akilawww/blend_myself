f632b77a5d45b2c5be0fc3d8f87ee45c98ce3d6e
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2019-01-23 14:49:42 +0900

    [Change] 投稿レシピがない時、ありませんを表示

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -13,29 +13,29 @@
   <h1>{{ Auth::user()->name }}のマイページ</h1>
   <hr>
   <h2>投稿レシピ</h2>
-  @if (isset($recipes))
-  @foreach ($recipes as $recipe)
-  <div class="container-fluid">
-    <a href="{{ url('/recipes', $recipe->id) }}">
-      <div class="card-horizon">
-        <div class="row card-horizon-con">
-          <div class="col-md-4 col-4 p-0 wh-100 left bg-secondary">
-            <img src="{{ asset($recipe->image) }}" alt="Sample">
+  @if (!$recipes->isEmpty())
+    @foreach ($recipes as $recipe)
+      <div class="container-fluid">
+        <a href="{{ url('/recipes', $recipe->id) }}">
+          <div class="card-horizon">
+            <div class="row card-horizon-con">
+              <div class="col-md-4 col-4 p-0 wh-100 left bg-secondary">
+                <img src="{{ asset($recipe->image) }}" alt="Sample">
+              </div>
+              <div class="col-md-8 p-0 wh-100 right bg-secondary">
+                <h5 class="card-title text-white">
+                  {{ $recipe->title }}
+                </h5>
+                <p class="card-text text-white">
+                  {{ $recipe->body }}
+                </p>
+              </div>
+            </div>
           </div>
-          <div class="col-md-8 p-0 wh-100 right bg-secondary">
-            <h5 class="card-title text-white">
-              {{ $recipe->title }}
-            </h5>
-            <p class="card-text text-white">
-              {{ $recipe->body }}
-            </p>
-          </div>
-        </div>
+        </a>
       </div>
-    </a>
-  </div>
-  <br>
-  @endforeach
+      <br>
+    @endforeach
     {{ $recipes->links() }}
   @else
     投稿されたレシピはありません
