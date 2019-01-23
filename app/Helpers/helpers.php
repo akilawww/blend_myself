c82b30a2279efcd2f9713192b5d8af74fbb4cd5a
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2019-01-23 14:50:35 +0900

    [New] Create function tagVerCount

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -11,3 +11,15 @@
         array_splice($ary, 0, 1, array('public')); // storageをpublicに変換
         return implode('/', $ary);
     }
+
+    function tagVerCount($tagVers, $tagCount){
+        $result = array();
+        foreach ($tagVers as $tagVer1){
+            $recipe = $tagVer1['recipe_id'];
+            $recipeCount = 0;
+            foreach ($tagVers as $tagVer2)
+                if ($tagVer2['recipe_id'] === $recipe) $recipeCount++;
+            if ($tagCount === $recipeCount) $result[] = $recipe;
+        }
+        return $result;
+    }
