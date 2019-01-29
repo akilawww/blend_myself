6eef31e932a58a412a0d31d67c766aeaca155863
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2019-01-29 14:30:15 +0900

    [ADD] verified routing

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@
 // レシピ一覧画面のルーティング
 Route::get('/', 'RecipesController@index');
 Route::get('/recipes/{id}', 'RecipesController@show');
-Auth::routes();
-Route::get('/home', 'RecipesController@index');
+//Route::get('/home', 'RecipesController@index');
 Route::get('/result','RecipesController@search');
 Route::get('/result/tag','RecipesController@searchTag');
+
+// 認証機能
+Auth::routes(['verify' => true]);
+Route::get('/home', 'RecipesController@index')->middleware('verified')->name('home');
 
 // ヘッダーのリンクのルーティング
 Route::get('/guideline', 'GuidesController@guideline');
@@ -26,50 +29,50 @@ Route::get('/contact', 'GuidesController@contact');
 
 // レシピ投稿画面のルーティング
 Route::get('/recipe_form', 'RecipeFormController@index')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::get('/recipe_form/materrial_procedure/{id}', 'RecipeFormController@materrialProcedure')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::post('/recipe_form/posts', 'RecipeFormController@store')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::post('/recipe_form/materrial/posts', 'RecipeFormController@materrialStore')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::post('/recipe_form/procedure/posts', 'RecipeFormController@procedureStore')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::post('/recipe_form/create/posts', 'RecipeFormController@create')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 
 // マイページのルーティング
 Route::get('/mypage', 'MyPageController@index')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 // 編集ページのルーティング
 Route::get('/edit/{id}', 'EditController@index')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::put('/edit/updateRecipe/{id}',  'EditController@updateRecipe')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::delete('/edit/deleteMaterrial/{id}',  'EditController@deleteMaterrial')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::delete('/edit/deleteProcedure/{id}',  'EditController@deleteProcedure')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::put('/edit/updateProcedure/{id}',  'EditController@updateProcedure')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 
 // お気に入り機能ルーティング
 Route::post('/favorite/add', 'FavoriteController@add')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::delete('/favorite/remove', 'FavoriteController@remove')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 
 // いいね機能ルーティング
 Route::post('/nice/add', 'NiceController@add')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::delete('/nice/remove', 'NiceController@remove')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 
 // フォロー機能ルーティング
 Route::post('/follow/add', 'FollowsController@add')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 Route::delete('/follow/remove', 'FollowsController@remove')
-    ->middleware('auth.basic');
+    ->middleware('verified');
 
 // ユーザーページのルーティング
 Route::get('/userpage/{id}', 'UserPageController@index');
