02fdfd6590fc2f28f08ebaa0e71618bb4673a0f7
KaoruBear <kd1192049@st.kobedenshi.ac.jp>
2019-01-31 10:08:33 +0900

    [Change]my recipes UI

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -14,7 +14,7 @@
   <hr>
   <h2 style="color:white;">投稿レシピ</h2>
   @if (!$recipes->isEmpty())
-    <div class="container bg-dark rounded" style="padding:1rem 1.1rem;margin: 1rem;">
+    <div class="container navbar-dark rounded" style="padding:1rem 1.1rem;margin: 1rem;">
   @foreach ($recipes as $recipe)
     <div class="container-fluid">
     <a href="{{ url('/recipes', $recipe->id) }}">
