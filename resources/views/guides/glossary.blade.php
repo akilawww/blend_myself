26054988eb978b7da41ba1e775f062df09cd2bb7
KaoruBear <kd1192049@st.kobedenshi.ac.jp>
2019-02-05 12:22:15 +0900

    [Change]content add

--- resources/views/guides/glossary.blade.php
+++ resources/views/guides/glossary.blade.php
@@ -6,8 +6,18 @@
 @include('navbar.header')
 
 @section('content')
-<div class="container-fluid" style="background: beige; margin-bottom:2rem;">
- 	<center><a href="{{ url('/')}}" class="text-center">{{ __('Topへ戻る') }}</a></center>
+<div class="container large">
+	<div class="row">
+	 <div class="col-md-2 glossary-sidebar"style="background:red; height:100%;">SideBar</div>
+	 <div class="col-md-10">
+			<div class ="glossary-cards rounded border" style ="font:30rem;"> <center>Glossarys</center> </div>
+		 <div class="container-fluid glossary-main rounded">
+			<div class="container-fluid" style="background: beige; margin:2rem 0rem;">
+		 	<center><a href="{{ url('/')}}" class="text-center">{{ __('Topへ戻る') }}</a></center>
+			</div>
+		 </div>
+	</div>
+	</div>
 </div>
 @endsection
 
